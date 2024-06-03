1eacd9bf5cb922a6d239f9afe7f126a7d4f61c34
FERRARINI ENZOm <ferrarini.enzo.nqn@gmail.com>
2024-06-03 19:37:21 -0300

    Test in Prod

--- src/util/api.js
+++ src/util/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import {transformNumberToDB} from "../util/validations.js"
+import { transformNumberToDB } from "../util/validations.js";
 
 // console.log("REACT_APP_BASE_URL_API:");
 // console.log(process.env.REACT_APP_BASE_URL_API);
@@ -19,7 +19,19 @@ export const getProducts = async (searchValue = "") => {
 export const postProducts = async (body) => {
   const formData = new FormData();
   Object.entries(body).forEach(([key, value]) => {
-    key==="price"? formData.append(key, transformNumberToDB(value)):formData.append(key, value);
+    if (key === "price") {
+      console.log("Key");
+      console.log(key);
+
+      console.log("Old Value");
+      console.log(value);
+      console.log("New Value");
+      console.log(transformNumberToDB(value));
+    }
+
+    key === "price"
+      ? formData.append(key, transformNumberToDB(value))
+      : formData.append(key, value);
   });
   const resp = await axiosInstance.post("/products", formData, {
     headers: {
